2c3cbe240f016bad50504ff61f3d57c8c7e0964c
OmniPot <r.conkov@gmail.com>
2015-05-26 03:13:06 +0300

    --

---
+++ Social-Network/js/controllers/AppController.js
@@ -0,0 +1,27 @@
+socialNetwork.controller('AppController', function($scope, authentication) {
+	$scope.authentication = authentication;
+
+
+	$scope.formatImageString = function(data) {
+		if (data) {
+			return 'data:image/jpeg;base64,' + data;
+		}
+		return null;
+	}
+
+	$scope.checkForImagesData = function(userData) {
+		if (userData.profileImageData == null) {
+			if (userData.gender == 'Male' || userData.gender == 0) {
+				userData.profileImageData = 'resources/defaultMale.jpg';
+			} else {
+				userData.profileImageData = 'resources/defaultFemale.jpg';
+			}
+		}
+
+		if (userData.coverImageData == null) {
+			userData.coverImageData = 'resources/defaultCover.jpg';
+		}
+
+		return userData;
+	}
+});
